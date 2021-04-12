0767e1ae7e2d6a05143f1ee22f3034f7e6c9a0b2
Mathew <matymcleod@gmail.com>
2021-04-12 00:02:16 +0000

    Initial commit with all my work so far

---
+++ object-keys.js
@@ -0,0 +1,28 @@
+/* 
+//Example
+const car = {  
+    make: 'Toyota',  
+    model: 'Camry',  
+    year: 2020  
+  }  
+  const keys = Object.keys(car)  
+//Instruction
+  console.log(keys);
+  
+  const car = {  
+    make: 'Honda',  
+    model: 'Accord',  
+    year: 2020  
+  } 
+  const keys = Object.keys(car)
+  console.log(keys);
+  */
+//My own creation
+const myName = {
+    First: 'Mathew',
+    Second: 'George',
+    Last: 'McLeod',
+    Age: 41
+}
+const myInfo = Object.keys(myName);
+console.log(myInfo);
